136f2c6997befc305dfa334c40f16eb28e4b7bb0
SayoranX <sayoran@mail.ru>
2025-02-28 21:16:30 +0500

    Printf %g %d

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
@@ -11,5 +12,9 @@ func main() {
 	fmt.Println("The time is", time.Now())
 	fmt.Println("My favorite number is", rand.Intn(10))
 	fmt.Println("My favorite number is", rand.Int())
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println("My favorite \n number")
+	fmt.Printf("Now you have %g problems gfhfnh bgfn %g hkhjkhjk dhgdgd %d dhhhnfj.\n", math.Sqrt(7), 25, math.Sqrt(7))
+	fmt.Printf("Now you have %g problems gfhfnh bgfn %d hkhjkhjk dhgdgd %g dhhhnfj.\n", math.Sqrt(7), rand.Intn(25), math.Sqrt(7))
 
 }
