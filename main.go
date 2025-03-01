319b6d5dfc40c46093961a3c30a01ee18fd52cf7
SayoranX <sayoran@mail.ru>
2025-03-01 17:15:14 +0500

    swap+split+bool+cmplx

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	//"math/rand"
+	"math/cmplx"
 )
 
 func main() {
@@ -17,12 +19,41 @@ func main() {
 	fmt.Println(math.Pi)
 	fmt.Println(add(42, 15))
 
-	x := 42
+	x := 41
 	y := 13
 	c := add(x, y)
 	fmt.Println(c)
+
+	a, b := swap("Smoky", "\nPanda") // задание с функцией swap вывод 2 строк
+	fmt.Println(a, b)
+
+	fmt.Println(split(20)) // задание с функцией split
+
+	var i, j int = 2, 4
+	v, python, java := false, true, "yes!"
+
+	fmt.Println(i, j, v, python, java) //  присвоение данных переменным влючая типы bool
+
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value %v\n", z, z)
 }
-func add(x int, y int) int {
+func add(x, y int) int {
 	c := x + y
 	return c
 }
+func swap(x, y string) (string, string) {
+	return x, y
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+var (
+	ToBe   bool       = true
+	MaxInt uint32     = 1<<32 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
